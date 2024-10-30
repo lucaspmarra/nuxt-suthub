7955995e4a0d73aa728ab7b8b1c9a018c3b6f3f7
Lucas Marra <marralucas1@gmail.com>
2024-10-30 19:29:00 -0300

    added app top bar and changes at styles

--- app.vue
+++ app.vue
@@ -1,6 +1,4 @@
 <script setup>
-import Index from "~/pages/index.vue";
-
 provideHeadlessUseId(() => useId())
 </script>
 
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+
+import AppTopBar from "~/layouts/components/AppTopBar.vue";
+</script>
+
+<template>
+  <div>
+    <AppTopBar/>
+    <div class="mt-1">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
