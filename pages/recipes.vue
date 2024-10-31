e4a8b4fec3ebe60a912206dbaf529ce0899415cf
Lucas Marra <marralucas1@gmail.com>
2024-10-31 13:52:09 -0300

    background of the recipes pages is now working

--- pages/recipes.vue
+++ pages/recipes.vue
@@ -83,7 +83,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <main class="flex items-center justify-center min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+  <main class="flex items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
     <div class="w-full max-w-3xl bg-white p-8 rounded-lg shadow-md">
       <h2 class="text-2xl font-bold mb-8 text-gray-900">Receitas</h2>
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -110,7 +110,7 @@ onMounted(async () => {
                   @after-leave="tagQuery = ''"
               >
                 <ComboboxOptions
-                    class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm"
+                    class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm z-10"
                 >
                   <div
                       v-if="filteredTags.length === 0 && tagQuery !== ''"
